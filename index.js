b1aac33b8030f1feee1e4573f7f8997fe441232f
chanchalkumar01 <141403710+chanchalkumar01@users.noreply.github.com>
2025-02-04 11:51:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,22 +110,17 @@ const YOUTUBE_COOKIES = `SID=g.a000swi9nuBuIOD8NaH6HM5fCrItDVQi81wVQYEEp9IhRflbL
                          SSID=AARFXg1JFi8-8Gd8q;
                          SAPISID=DdiZ3gtH9ouI1ejA/A32qFJBLS9aaYFe24`;
 
-const HEADERS = {
-    "Cookie": "LOGIN_INFO=AFmmF2swRQIhAKtsWPiwExjgM1FYq5PwOA5iMDqdJlEZLdJu6NM_sn2nAiA2geNj8jzX5phANMmmfr3wWezyfQ4Zl0-umU8nE3uUOw:QUQ3MjNmeFRqUk40NGlDeS0xUWJUNnN5RlVNc0pRNHdpSFlvUnhVRW56a3hULVB5Z1E1Rmd0YjBpejRicFdUeGl4NDJVeGhyRmFTTFZLZDFkbV85YU9UdER3cTRGZjZtUVhmdlp5ckJMZGJUcDNEbUZmdzdER1FkaXNyVWF0Ul80N3FMcl90QVVhVkJqV0FWNEUwQk5wcnV4VHJPUVJoSll3; VISITOR_INFO1_LIVE=FWV44X0mXLc; VISITOR_PRIVACY_METADATA=CgJJThIEGgAgEg%3D%3D; YSC=ezEaAmqhjt4; HSID=AbwDidialpIflgej7; SSID=AARFXg1JFi8-8Gd8q; APISID=VR1iB1DKWACzj0Og/AocfbdUDt4yEFePTG; SAPISID=DdiZ3gtH9ouI1ejA/A32qFJBLS9aaYFe24; __Secure-1PAPISID=DdiZ3gtH9ouI1ejA/A32qFJBLS9aaYFe24; __Secure-3PAPISID=DdiZ3gtH9ouI1ejA/A32qFJBLS9aaYFe24; SID=g.a000swi9nuBuIOD8NaH6HM5fCrItDVQi81wVQYEEp9IhRflbL-3Eb3-Nq6pxUSHQxYTNvlvzOgACgYKAb4SARQSFQHGX2MiorOSG8bzEh6zqJyF8FkFLBoVAUF8yKq5iYJIgvZhxt6-_2F86BvO0076; __Secure-1PSID=g.a000swi9nuBuIOD8NaH6HM5fCrItDVQi81wVQYEEp9IhRflbL-3EwYqk-7GpAdPmLozlvlzzewACgYKAS4SARQSFQHGX2MiPqu9tYOpWKIyBG4YRCY1ExoVAUF8yKqrFmXkvpk7RhGaeXnYqLag0076; __Secure-3PSID=g.a000swi9nuBuIOD8NaH6HM5fCrItDVQi81wVQYEEp9IhRflbL-3EpASEy7PuBSScOMMsurhkaAACgYKAQESARQSFQHGX2Miyi2W8ebnZJ5qTrGzQ1fsrBoVAUF8yKrnvqNtmlvE73U1gc0rk-Nr0076; PREF=f4=4000000&tz=Asia.Calcutta&f5=20000&f6=40000000&f7=18100; __Secure-ROLLOUT_TOKEN=CPDS9e7jzu7NqgEQ1MH1urjqigMYj9jm1qepiwM%3D; __Secure-1PSIDTS=sidts-CjIBmiPuTavzk7gWBgopVZA7yv5JTz-yW2aCdiHYCA1swqim5E7EJEuw55Nb9tEccRanPBAA; __Secure-3PSIDTS=sidts-CjIBmiPuTavzk7gWBgopVZA7yv5JTz-yW2aCdiHYCA1swqim5E7EJEuw55Nb9tEccRanPBAA; ST-3opvp5=session_logininfo=AFmmF2swRQIhAKtsWPiwExjgM1FYq5PwOA5iMDqdJlEZLdJu6NM_sn2nAiA2geNj8jzX5phANMmmfr3wWezyfQ4Zl0-umU8nE3uUOw%3AQUQ3MjNmeFRqUk40NGlDeS0xUWJUNnN5RlVNc0pRNHdpSFlvUnhVRW56a3hULVB5Z1E1Rmd0YjBpejRicFdUeGl4NDJVeGhyRmFTTFZLZDFkbV85YU9UdER3cTRGZjZtUVhmdlp5ckJMZGJUcDNEbUZmdzdER1FkaXNyVWF0Ul80N3FMcl90QVVhVkJqV0FWNEUwQk5wcnV4VHJPUVJoSll3; SIDCC=AKEyXzUK1tnPjxa5aj7HB1AjZIukhyWOGHE5JQO-YFWtzri_7Wp47AgbgRwfO39tlyLUZc6UmQ; __Secure-1PSIDCC=AKEyXzVRJhMhIFlENeCspQFSkmhJKDj-05JXVyZ8o_gDYouzmJ_1PdK3Eim_uBUSKxn2p0XfsQ; __Secure-3PSIDCC=AKEyXzWlo9WNUbH4ubyy1MFKXc21-BPtXoYsn6vPu37qaIitIo7Sb-NwbGQZsr5OXFNrvu3onAA",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36\n",
-    "X-Origin": "https://www.youtube.com",
-    "Referer": "https://www.youtube.com"
-};
-
 app.get("/get-form-options", async (req, res) => {
-    const { url } = req.query;
+    const {url} = req.query;
     if (!url) {
         return res.status(400).send("<h1>Please provide a YouTube video URL</h1>");
     }
 
     try {
         const videoInfo = await ytdl.getInfo(url, {
-            requestOptions: { headers: HEADERS }
+            requestOptions: {
+                headers: {Cookie: YOUTUBE_COOKIES},
+            },
         });
 
         const seenQualities = new Set();
